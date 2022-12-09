c1337e60c39a202a9a235852cfade7ba8613745c
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2022-12-08 22:04:51 -0300

    added Personalidade

--- src/main/java/br/edu/infnet/gerenciadorpersonagens/model/domain/Personalidade.java
+++ src/main/java/br/edu/infnet/gerenciadorpersonagens/model/domain/Personalidade.java
@@ -1,9 +1,121 @@
 package br.edu.infnet.gerenciadorpersonagens.model.domain;
 
+import java.util.ArrayList;
+
 public class Personalidade extends Caracteristica{
+
+    private String palavraDefinicao;
+    private String tipoExpressao;
+    private String religiao;
+    private String temperamento;
+    private ArrayList<String> qualidades;
+    private  ArrayList<String> defeitos;
+    private ArrayList<String> interesses;
+    private ArrayList<String> desinteresses;
+
+    public Personalidade(
+            String descricao,
+            String intensidade,
+            String inspiracao,
+            String palavraDefinicao,
+            String tipoExpressao,
+            String religiao,
+            String temperamento,
+            ArrayList<String> qualidades,
+            ArrayList<String> defeitos,
+            ArrayList<String> interesses,
+            ArrayList<String> desinteresses
+    ) {
+        super(descricao,intensidade,inspiracao);
+        this.palavraDefinicao = palavraDefinicao;
+        this.tipoExpressao = tipoExpressao;
+        this.religiao = religiao;
+        this.temperamento = temperamento;
+        this.qualidades = qualidades;
+        this.defeitos = defeitos;
+        this.interesses = interesses;
+        this.desinteresses = desinteresses;
+    }
 
     @Override
     public String formatDescricao() {
-        return null;
+        return '"' + super.getDescricao() + '"';
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + ";" + palavraDefinicao
+                + ";" + tipoExpressao
+                + ";" + religiao
+                + ";" + temperamento
+                + ";" + qualidades
+                + ";" + defeitos
+                + ";" + interesses
+                + ";" + desinteresses;
+    }
+
+    public String getTipoExpressao() {
+        return tipoExpressao;
+    }
+
+    public void setTipoExpressao(String tipoExpressao) {
+        this.tipoExpressao = tipoExpressao;
+    }
+
+    public String getReligiao() {
+        return religiao;
+    }
+
+    public void setReligiao(String religiao) {
+        this.religiao = religiao;
+    }
+
+    public String getTemperamento() {
+        return temperamento;
+    }
+
+    public void setTemperamento(String temperamento) {
+        this.temperamento = temperamento;
+    }
+
+    public ArrayList<String> getQualidades() {
+        return qualidades;
+    }
+
+    public void setQualidades(ArrayList<String> qualidades) {
+        this.qualidades = qualidades;
+    }
+
+    public ArrayList<String> getDefeitos() {
+        return defeitos;
+    }
+
+    public void setDefeitos(ArrayList<String> defeitos) {
+        this.defeitos = defeitos;
+    }
+
+    public ArrayList<String> getInteresses() {
+        return interesses;
+    }
+
+    public void setInteresses(ArrayList<String> interesses) {
+        this.interesses = interesses;
+    }
+
+    public ArrayList<String> getDesinteresses() {
+        return desinteresses;
+    }
+
+    public void setDesinteresses(ArrayList<String> desinteresses) {
+        this.desinteresses = desinteresses;
+    }
+
+    public String getPalavraDefinicao() {
+        return palavraDefinicao;
+    }
+
+    public void setPalavraDefinicao(String palavraDefinicao) {
+        this.palavraDefinicao = palavraDefinicao;
     }
 }
---
+++ src/main/java/br/edu/infnet/gerenciadorpersonagens/model/tests/PersonalidadeTeste.java
@@ -0,0 +1,27 @@
+package br.edu.infnet.gerenciadorpersonagens.model.tests;
+
+import br.edu.infnet.gerenciadorpersonagens.model.domain.Personalidade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonalidadeTeste {
+    public static void main(String[] args) {
+        Personalidade person1 = new Personalidade(
+            "É um rapaz confiante e observador, com desejo de explorar o mundo e conhecer seus próprios" +
+                    " limites",
+            "Muito presente",
+            "Han Solo",
+            "Explorador",
+            "Introvertido",
+            "não definida",
+            "agressivo",
+            new ArrayList<>(List.of("Compasivo", "Caridoso", "Generoso", "Cordial", "Leal")),
+            new ArrayList<>(List.of("Impaciente", "Vingativo", "Raivoso")),
+            new ArrayList<>(List.of("Videogame", "Esportes", "Sair com os amigos")),
+            new ArrayList<>(List.of("Carros", "Televisao", "Leitura", "Música"))
+        );
+
+        System.out.println(person1);
+    }
+}
